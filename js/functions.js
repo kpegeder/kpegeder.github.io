97352a4ed68cd9554c97d59ca13f25b44db3dc98
kpegeder <k.pegeder@gmail.com>
2021-01-21 16:05:13 -0800

    add links to projects

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  const loadMore = document.getElementById("load-more-projects");
+  console.log($(".h_gallery_item"));
+  $(".h_gallery_item").slice(0, 6).slideDown();
+  loadMore.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    console.log("hi");
+    $("div:hidden").slice(0, 6).slideDown();
+    if ($("div:hidden").length == 0) {
+      loadMore.hide();
+    }
+  });
+});
